bb1e87d0ca0a99bbc1ed764b691f1111d160343e
Alex <AlexGrimes.se@gmail.com>
2023-02-02 12:46:58 -0500

    adding read and close for body of resp

--- basic/main.go
+++ basic/main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func main() {
@@ -25,4 +27,19 @@ func main() {
 	if err != nil {
 		log.Panicln(err)
 	}
+	resp.Body.Close()
+	fmt.Println(resp.Status)
+
+	form := url.Values{}
+	form.Add("foo", "bar")
+	resp, err = http.Post(
+		"https://www.google.com/robots.txt",
+		"application/x-www-form-urlencoded",
+		strings.NewReader(form.Encode()),
+	)
+	if err != nil {
+		log.Panicln(err)
+	}
+	resp.Body.Close()
+	fmt.Println(resp.Status)
 }
